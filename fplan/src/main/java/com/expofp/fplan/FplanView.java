903c007c7c31dbc6f7db804ffe2af826738cec25
Nikitin Vladimir <vladimir.nikitin@shtormtech.ru>
2022-04-28 15:58:43 +0300

    1.1.6 release

--- fplan/src/main/java/com/expofp/fplan/FplanView.java
+++ fplan/src/main/java/com/expofp/fplan/FplanView.java
@@ -1,29 +1,42 @@
 package com.expofp.fplan;
 
+import static android.webkit.WebSettings.MIXED_CONTENT_ALWAYS_ALLOW;
+
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.net.ConnectivityManager;
+import android.net.Uri;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.webkit.JavascriptInterface;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.FrameLayout;
 
 import androidx.annotation.Nullable;
 
 import org.json.JSONException;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * View for displaying expo plans
  */
 public class FplanView extends FrameLayout {
 
-    private WebView _webView;
-    private FplanEventListener _eventListener;
+    private WebView webView;
+    private FplanEventListener eventListener;
+    private ConnectivityManager connectivityManager;
 
     /**
      * Constructor
@@ -76,32 +89,130 @@ public class FplanView extends FrameLayout {
      * @param eventListener Events listener
      */
     public void init(String url, Boolean noOverlay, @Nullable FplanEventListener eventListener) {
-        _eventListener = eventListener;
+        this.eventListener = eventListener;
 
         String eventId = getEventId(url);
         String baseUrl = url.substring(0, url.indexOf(".expofp.com") + 11);
+        String params = "";
+        if(url.contains("?")){
+            params = url.substring(url.indexOf("?"));
+        }
 
-        _webView.post(() -> {
-            String html = "";
-            try {
-                InputStream inputStream = this.getContext().getAssets().open("index.html");
-                byte[] buffer = new byte[inputStream.available()];
-                inputStream.read(buffer);
-                html = new String(buffer);
-                html = html.replace("$url#", baseUrl).replace("$eventId#", eventId).replace("$noOverlay#", noOverlay.toString());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        File cacheDir = getContext().getFilesDir();
+        File fPlanCacheDir = new File(cacheDir, "fplan");
+        if(fPlanCacheDir.exists()){
+            fPlanCacheDir.delete();
+        }
 
-            ConnectivityManager cm = (ConnectivityManager) this.getContext().getSystemService(Activity.CONNECTIVITY_SERVICE);
-            if (cm != null && cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected()) {
-                _webView.clearCache(true);
-                _webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
-            } else {
-                _webView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
-            }
-            _webView.loadDataWithBaseURL(url, html, "text/html", "en_US", null);
-        });
+        File expoCacheDir = new File(fPlanCacheDir, eventId);
+        File indexFilePath = new File(expoCacheDir, "index.html");
+
+        String html = "";
+        try {
+            InputStream inputStream = this.getContext().getAssets().open("index.html");
+            byte[] buffer = new byte[inputStream.available()];
+            inputStream.read(buffer);
+            html = new String(buffer);
+            html = html.replace("$url#", "file:///" + expoCacheDir.getAbsolutePath()).replace("$eventId#", eventId).replace("$noOverlay#", noOverlay.toString());
+            Helper.writeToFile(indexFilePath, html.getBytes(StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String finalParams = params;
+        Context context = this.getContext();
+
+        if (connectivityManager != null && connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnected()) {
+
+            ReentrantLock locker = new ReentrantLock();
+            AtomicBoolean cached = new AtomicBoolean(false);
+            AtomicBoolean loaded = new AtomicBoolean(false);
+
+            webView.post(() -> {
+                webView.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
+                webView.setWebViewClient(new WebViewClient(){
+                    @Nullable
+                    @Override
+                    public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
+                        if(request.getUrl().getScheme().equalsIgnoreCase("file")){
+                            String reqPath = request.getUrl().getPath().replace(expoCacheDir.getAbsolutePath(), "");
+                            if(reqPath.startsWith("/data/exhibitors/")){
+                                String reqUrl = baseUrl + reqPath;
+                                Helper.updateFile(reqUrl, new File(request.getUrl().getPath()));
+                            }
+                        }
+
+                        return super.shouldInterceptRequest(view, request);
+                    }
+
+                    @Nullable
+                    @Override
+                    public void onPageFinished(WebView view, String url) {
+                        locker.lock();
+
+                        loaded.set(true);
+                        if(cached.get() && loaded.get()) {
+                            initFloorplan();
+                        }
+
+                        locker.unlock();
+                    }
+
+                    @Nullable
+                    @Override
+                    public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request){
+                        try {
+                            context.startActivity(new Intent(Intent.ACTION_VIEW, request.getUrl()));
+                        }
+                        catch (Exception ex){
+                            ex.printStackTrace();
+                        }
+                        return true;
+                    }
+                });
+                webView.setWebChromeClient(new WebChromeClient());
+                webView.loadUrl("file:///" + indexFilePath.getAbsolutePath() + finalParams);
+            });
+
+            Helper.updateCache(expoCacheDir, baseUrl, () -> {
+                locker.lock();
+
+                cached.set(true);
+                if(cached.get() && loaded.get()){
+                    webView.post(() -> {
+                        initFloorplan();
+                    });
+                }
+
+                locker.unlock();
+            });
+        }
+        else {
+            webView.post(() -> {
+                webView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ONLY);
+                webView.setWebViewClient(new WebViewClient(){
+                    @Nullable
+                    @Override
+                    public void onPageFinished(WebView view, String url) {
+                        initFloorplan();
+                    }
+
+                    @Nullable
+                    @Override
+                    public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request){
+                        try {
+                            context.startActivity(new Intent(Intent.ACTION_VIEW, request.getUrl()));
+                        }
+                        catch (Exception ex){
+                            ex.printStackTrace();
+                        }
+                        return true;
+                    }
+                });
+                webView.setWebChromeClient(new WebChromeClient());
+                webView.loadUrl("file:///" + indexFilePath.getAbsolutePath() + finalParams);
+            });
+        }
     }
 
     /**
@@ -110,9 +221,9 @@ public class FplanView extends FrameLayout {
      * @param boothName Booth name or externalID
      */
     public void selectBooth(String boothName) {
-        _webView.post(() -> {
+        webView.post(() -> {
             String js = String.format("selectBooth('%s')", boothName);
-            _webView.evaluateJavascript(js, null);
+            webView.evaluateJavascript(js, null);
         });
     }
 
@@ -134,9 +245,9 @@ public class FplanView extends FrameLayout {
      * @param focus True - focus on a point
      */
     public void setCurrentPosition(int x, int y, boolean focus) {
-        _webView.post(() -> {
+        webView.post(() -> {
             String js = String.format("setCurrentPosition(%d, %d, %b)", x, y, focus);
-            _webView.evaluateJavascript(js, null);
+            webView.evaluateJavascript(js, null);
         });
     }
 
@@ -158,9 +269,9 @@ public class FplanView extends FrameLayout {
      * @param exceptInaccessible True - exclude routes that are inaccessible to people with disabilities, False - include all routes
      */
     public void buildRoute(String from, String to, boolean exceptInaccessible) {
-        _webView.post(() -> {
+        webView.post(() -> {
             String js = String.format("selectRoute('%s', '%s', %b)", from, to, exceptInaccessible);
-            _webView.evaluateJavascript(js, null);
+            webView.evaluateJavascript(js, null);
         });
     }
 
@@ -168,16 +279,21 @@ public class FplanView extends FrameLayout {
      * Clear floor plan
      */
     public void clear() {
-        _webView.post(() -> {
+        webView.post(() -> {
             String js = String.format("selectRoute(null, null, false)");
-            _webView.evaluateJavascript(js, null);
+            webView.evaluateJavascript(js, null);
 
             js = String.format("setCurrentPosition(null, null, false)");
-            _webView.evaluateJavascript(js, null);
+            webView.evaluateJavascript(js, null);
         });
     }
 
-    private  String getEventId(String url) {
+    private void initFloorplan() {
+        String js = String.format("init()");
+        webView.evaluateJavascript(js, null);
+    }
+
+    private String getEventId(String url) {
         String baseUrl = url.replace("http://", "")
                 .replace("https://", "").replace("www.", "");
 
@@ -186,45 +302,57 @@ public class FplanView extends FrameLayout {
     }
 
     private void initView(Context context) {
-        _webView = new WebView(this.getContext());
+        webView = new WebView(this.getContext());
+        connectivityManager = (ConnectivityManager) this.getContext().getSystemService(Activity.CONNECTIVITY_SERVICE);
 
         FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams
                 (FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
         layoutParams.gravity = Gravity.TOP | Gravity.RIGHT | Gravity.BOTTOM | Gravity.LEFT;
 
-        _webView.setLayoutParams(layoutParams);
+        webView.setLayoutParams(layoutParams);
 
-        _webView.getSettings().setJavaScriptEnabled(true);
-        _webView.getSettings().setDomStorageEnabled(true);
-        _webView.getSettings().setAllowFileAccess(true);
-        _webView.getSettings().setAppCacheEnabled(true);
-        _webView.getSettings().setAppCacheMaxSize(1024*1024*100);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setAllowFileAccess(true);
+        webView.getSettings().setAllowFileAccessFromFileURLs(true);
+        webView.getSettings().setAllowUniversalAccessFromFileURLs(true);
 
-        _webView.addJavascriptInterface(new Object() {
+        webView.getSettings().setAllowContentAccess(true);
+        webView.getSettings().setMixedContentMode(MIXED_CONTENT_ALWAYS_ALLOW);
+
+        webView.getSettings().setAppCacheEnabled(true);
+        File dir = context.getCacheDir();
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        webView.getSettings().setAppCachePath(dir.getPath());
+        webView.getSettings().setDatabaseEnabled(true);
+
+        webView.addJavascriptInterface(new Object() {
             @JavascriptInterface
             public void postMessage(String message) {
-                if (_eventListener != null) {
-                    _eventListener.onFpConfigured();
+                if (eventListener != null) {
+                    eventListener.onFpConfigured();
                 }
             }
         }, "onFpConfiguredHandler");
 
-        _webView.addJavascriptInterface(new Object() {
+        webView.addJavascriptInterface(new Object() {
             @JavascriptInterface
             public void postMessage(String boothName) {
-                if (_eventListener != null) {
-                    _eventListener.onBoothSelected(boothName);
+                if (eventListener != null) {
+                    eventListener.onBoothSelected(boothName);
                 }
             }
         }, "onBoothClickHandler");
 
-        _webView.addJavascriptInterface(new Object() {
+        webView.addJavascriptInterface(new Object() {
             @JavascriptInterface
             public void postMessage(String directionJson) throws JSONException {
-                if (_eventListener != null) {
+                if (eventListener != null) {
                     try {
                         Route route = Helper.parseRoute(directionJson);
-                        _eventListener.onRouteCreated(route);
+                        eventListener.onRouteCreated(route);
                     } catch (JSONException e) {
                         throw e;
                     }
@@ -232,6 +360,6 @@ public class FplanView extends FrameLayout {
             }
         }, "onDirectionHandler");
 
-        this.addView(_webView);
+        this.addView(webView);
     }
 }
--- fplan/src/main/java/com/expofp/fplan/Helper.java
+++ fplan/src/main/java/com/expofp/fplan/Helper.java
@@ -4,9 +4,111 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Helper {
+
+    private static final Map<String, String> cachingFiles = new HashMap<String, String>();
+
+    static {
+        cachingFiles.put("data/fp.svg.js", "data/fp.svg.js");
+        cachingFiles.put("data/wf.data.js", "data/wf.data.js");
+        cachingFiles.put("data/data.js", "data/data.js");
+        cachingFiles.put("data/demo.png", "data/demo.png");
+
+        cachingFiles.put("packages/master/vendors~floorplan.js", "vendors~floorplan.js");
+        cachingFiles.put("packages/master/floorplan.js", "floorplan.js");
+        cachingFiles.put("packages/master/expofp.js", "expofp.js");
+        cachingFiles.put("packages/master/free.js", "free.js");
+        cachingFiles.put("packages/master/slider.js", "slider.js");
+        cachingFiles.put("packages/master/expofp-overlay.png", "expofp-overlay.png");
+
+        cachingFiles.put("packages/master/fonts/oswald-v17-cyrillic_latin-300.woff2", "fonts/oswald-v17-cyrillic_latin-300.woff2");
+        cachingFiles.put("packages/master/fonts/oswald-v17-cyrillic_latin-500.woff2", "fonts/oswald-v17-cyrillic_latin-500.woff2");
+
+        cachingFiles.put("packages/master/vendor/fa/css/fontawesome-all.min.css", "vendor/fa/css/fontawesome-all.min.css");
+        cachingFiles.put("packages/master/vendor/fa/webfonts/fa-brands-400.woff2", "vendor/fa/webfonts/fa-brands-400.woff2");
+        cachingFiles.put("packages/master/vendor/fa/webfonts/fa-light-300.woff2", "vendor/fa/webfonts/fa-light-300.woff2");
+        cachingFiles.put("packages/master/vendor/fa/webfonts/fa-regular-400.woff2", "vendor/fa/webfonts/fa-regular-400.woff2");
+        cachingFiles.put("packages/master/vendor/fa/webfonts/fa-solid-900.woff2", "vendor/fa/webfonts/fa-solid-900.woff2");
+        cachingFiles.put("packages/master/vendor/perfect-scrollbar/css/perfect-scrollbar.css", "vendor/perfect-scrollbar/css/perfect-scrollbar.css");
+        cachingFiles.put("packages/master/vendor/sanitize-css/sanitize.css", "vendor/sanitize-css/sanitize.css");
+
+        cachingFiles.put("packages/master/locales/ar.json", "locales/ar.json");
+        cachingFiles.put("packages/master/locales/de.json", "locales/de.json");
+        cachingFiles.put("packages/master/locales/es.json", "locales/es.json");
+        cachingFiles.put("packages/master/locales/fr.json", "locales/fr.json");
+        cachingFiles.put("packages/master/locales/it.json", "locales/it.json");
+        cachingFiles.put("packages/master/locales/ko.json", "locales/ko.json");
+        cachingFiles.put("packages/master/locales/nl.json", "locales/nl.json");
+        cachingFiles.put("packages/master/locales/pt.json", "locales/pt.json");
+        cachingFiles.put("packages/master/locales/ru.json", "locales/ru.json");
+        cachingFiles.put("packages/master/locales/sv.json", "locales/sv.json");
+        cachingFiles.put("packages/master/locales/th.json", "locales/th.json");
+        cachingFiles.put("packages/master/locales/tr.json", "locales/tr.json");
+        cachingFiles.put("packages/master/locales/vi.json", "locales/vi.json");
+        cachingFiles.put("packages/master/locales/zh.json", "locales/zh.json");
+    }
+
+    public static void updateCache(File expoCacheDir, String baseUrl, Runnable callback) {
+        ReentrantLock locker = new ReentrantLock();
+        AtomicInteger count = new AtomicInteger();
+
+        for (String key : cachingFiles.keySet()) {
+            new Thread(() -> {
+                updateFile(baseUrl + "/" + key, new File(expoCacheDir, cachingFiles.get(key)));
+
+                locker.lock();
+                count.getAndIncrement();
+                if(count.get() == cachingFiles.size()){
+                    callback.run();
+                    count.set(0);
+                }
+                locker.unlock();
+
+            }).start();
+        }
+    }
+
+    public static void updateFile(String url, File filePath) {
+        try {
+            if(!filePath.exists()) {
+                filePath.getParentFile().mkdirs();
+                filePath.createNewFile();
+            }
+
+            InputStream in = new URL(url).openStream();
+            Files.copy(in, filePath.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void writeToFile(File file, byte[] data) throws IOException {
+        if(!file.exists()) {
+            file.getParentFile().mkdirs();
+            file.createNewFile();
+        }
+
+        FileOutputStream fs = new FileOutputStream(file);
+        fs.write(data);
+        fs.close();
+    }
+
     public static Route parseRoute(String json) throws JSONException {
         JSONObject jObject = new JSONObject(json);
 
